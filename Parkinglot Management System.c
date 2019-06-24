0a2be277aa852569f788435d6e53700547bebe16
PlanC <planc2333@outlook.com>
2019-06-24 17:20:45 +0800

    基础功能 & 登录
    PlanC : password

---
+++ Parkinglot Management System.c
@@ -0,0 +1,23 @@
+#define DEBUG
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "function.h"
+
+int main(int argc, char* argv[]) {
+	
+	printf("+------------------------+\n");
+	printf("| 欢迎来到停车场管理系统 |\n");
+	printf("+------------------------+\n");
+
+	if (login() != 0) {
+		printf("密码错误已至三次，请核对后重新运行。");
+		return 1;
+	}
+	printf("登录成功，按任意键继续......\n");
+	getchar();
+	system("cls");
+	printf("输入 \"help\" 以显示指令集\n");
+	while (statues() == 1);
+	return 0;
+}
---
+++ function.h
@@ -0,0 +1,87 @@
+#define DEBUG
+#define totalspace			100
+#define realusername		"PlanC"
+#define realuserpassword	"password"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int cars = 0;
+int spacepointer = 0;
+
+struct parkingspace {
+	int time;
+	int parkserial;
+	char carserial[5];
+};
+
+struct Parkinglot {
+	struct parkingspace ParkingSpace[totalspace];
+} Parkinglot;
+
+int login() {
+	int i = 0;
+	int retrytimes = 3;
+	char inputusername[10] = {"\0"};
+	char inputpassword[15] = {"\0"};
+
+#ifdef RELEASE
+	while (retrytimes--) {
+		printf("请输入用户名:");
+		scanf("%s", inputusername);
+		if (strcmp(inputusername, realusername) != 0) {
+			if (retrytimes == 3) {
+				printf("三次错误，程序退出。");
+				return 1;
+			}
+			continue;
+		}
+		else {
+			break;
+		}
+	}
+	
+	while (retrytimes--) {
+		printf("请输入密码:");
+		for (i = 0; i < 8; i++) inputpassword = getch();
+		inputpassword[8] = "\0";
+		if (strcmp(inputpassword, realuserpassword) != 0) {
+			if (retrytimes == 3) {
+				printf("三次错误，程序退出");
+				return 2;
+			}
+			continue;
+		}
+		else {
+			break;
+		}
+	}
+#endif
+	return 0;
+}
+
+int statues() {
+	char command[20] = {"\0"};
+	printf("PMS >");
+	scanf("%s", command);
+	if (strcmp(command, "park") == 0) {
+		cars++;
+		spacepointer++;
+		if (spacepointer != totalspace) {
+			sscanf(command, "%*s%s", Parkinglot.ParkingSpace[spacepointer].carserial);
+		}
+		return 3;
+	}
+	if (strcmp(command, "help") == 0) {
+		printf("\texit - 退出此程序\n");
+		printf("\thelp - 输出此列表\n");
+		printf("\tjump - 将时间增加一小时\n");
+		printf("\tpark - 新增入库车辆\n");
+		return 2;
+	}
+	if (strcmp(command, "exit") == 0) {
+		printf("程序已退出\n");
+		return 1;
+	}
+	return 0;
+}
